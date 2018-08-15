38da9950502f6de4aed8505563c432b601e809e4
liujiming <460667926@qq.com>
2018-08-15 09:04:26 +0000

    Basic user profile editing

---
+++ database/migrations/2018_08_14_164426_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAvatarAndIntroductionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable()->comment('頭像');
+            $table->string('introduction')->nullable()->comment('介紹');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('introduction');
+        });
+    }
+}
