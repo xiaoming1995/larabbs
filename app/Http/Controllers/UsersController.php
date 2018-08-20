4c92889138c52b0c4939b7c9db90d0a4835be6bc
liujiming <460667926@qq.com>
2018-08-20 02:37:50 +0000

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
+
 
 class UsersController extends Controller
 {
@@ -19,9 +21,18 @@ class UsersController extends Controller
     	return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $Request,User $user)
-    { 
-    	$user->update($Request->all());
+    public function update(UserRequest $Request,ImageUploadHandler $uploader,User $user)
+    { 	
+    	// dd($Request->avatar);
+	    $data = $Request->all();
+	   if ($Request->avatar) {
+            $result = $uploader->save($Request->avatar, 'avatars', $user->id,362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+    	$user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 
