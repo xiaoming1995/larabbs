3202e634671d0fdb177df1c434a1bef401e60fab
liujiming <460667926@qq.com>
2018-08-24 08:13:09 +0000

    Topic detail page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -25,7 +25,8 @@ class TopicsController extends Controller
 	}
 
     public function show(Topic $topic)
-    {
+    {	
+
         return view('topics.show', compact('topic'));
     }
 
@@ -41,7 +42,7 @@ class TopicsController extends Controller
 		$topic->user_id =  Auth::id();
 		$topic->save();
 		// $topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '创建话题成功');
 	}
 
 	public function edit(Topic $topic)
@@ -55,7 +56,7 @@ class TopicsController extends Controller
 	{
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功!');
 	}
 
 	public function destroy(Topic $topic)
@@ -63,7 +64,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '成功删除!');
 	}
 
 	public function uploadImage(Request $request,ImageUploadHandler $uploader)
