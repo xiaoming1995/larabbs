74cad3e160a86440430a1954d3ce21b939ddd3e4
liujiming <460667926@qq.com>
2018-08-29 08:54:22 +0000

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-
+use Auth;
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable{
+        notify as protected laravelNotify;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -46,4 +48,25 @@ class User extends Authenticatable
     }
 
 
+    //重写了这个函数 这个函数原本是有自带的
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        $this->increment('notification_count');
+
+        $this->laravelNotify($instance);
+    }
+
+     public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
+
+    
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,8 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
+
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -16,7 +18,11 @@ class ReplyObserver
 
     public function created(Reply $reply)
     { 
+    	$topic = $reply->topic;
     	$reply->topic->increment('reply_count',1);
+    	
+    	//通知作者话题被回复了
+    	$topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
