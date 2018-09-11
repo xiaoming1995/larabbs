59e18f65d361b225bdc3e112f2d1e288ccb2f7fe
liujiming <460667926@qq.com>
2018-09-11 07:54:26 +0000

    Sidebar Links

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,11 +6,11 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Topic;
 use APP\Models\User;
-
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category,Request $request,Topic $topic,User $user)
+    public function show(Category $category,Request $request,Topic $topic,User $user,Link $link)
     { 
     	//读取分类 ID 关联的话题.并按每20条分页
     	$topics = $topic->WithOrder($request->order)
@@ -20,8 +20,11 @@ class CategoriesController extends Controller
     	 // 活跃用户列表
         $active_users = $user->getActiveUsers();
 
+        //推荐链接
+        $links = $link->getAllCache();
+
 
     	//传参变量话题和分类到模板中
-    	return view('topics.index',compact('topics','category','active_users'));
+    	return view('topics.index',compact('topics','category','active_users','links'));
     }
 }
--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,8 +9,8 @@ use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use App\Handlers\ImageUploadHandler;
 use Auth;
-
 use App\Models\User;
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -19,13 +19,15 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request,Topic $topic,User $user)
+	public function index(Request $request,Topic $topic,User $user,Link $link)
 	{
 		// $topics = Topic::with('user','category')->paginate(30);
 		$topics = $topic->withOrder($request->order)->paginate(20);
 		$active_users = $user->getActiveUsers();
-		
-		return view('topics.index', compact('topics','active_users'));
+		$links = $link->getAllCached();
+
+
+		return view('topics.index', compact('topics','active_users','links'));
 	}
 
     public function show(Request $request,Topic $topic)
